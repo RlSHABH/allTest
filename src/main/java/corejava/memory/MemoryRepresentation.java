9b5335267c9854d7f1597360509af3f95ae6a70b
rishabh <rishab.bajpai@impressico.com>
2019-11-11 20:15:56 +0530

    changes

---
+++ src/main/java/corejava/memory/MemoryRepresentation.java
@@ -0,0 +1,20 @@
+package corejava.memory;
+
+public class MemoryRepresentation {
+
+    public static void main(String[] args) { // Line 1
+      int i=1; // Line 2
+      Object obj = new Object(); // Line 3
+      MemoryRepresentation mem = new MemoryRepresentation(); // Line 4
+      mem.foo(obj); // Line 5
+      System.out.println(obj);
+    } // Line 9
+
+    private void foo(Object param) { // Line 6
+    //  String str = param.toString(); //// Line 7
+      System.out.println(param);
+    } // Line 8
+
+  }
+
+
