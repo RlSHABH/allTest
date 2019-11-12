6949e094d4868e15e5c2c422e84400656f89faa2
rishabh <rishab.bajpai@impressico.com>
2019-11-12 18:53:20 +0530

    synchroniztion-example

---
+++ src/main/java/com/demo/corejava/multithreading/EvenOddNumbersThreads.java
@@ -0,0 +1,48 @@
+package com.demo.corejava.multithreading;
+
+public class EvenOddNumbersThreads {
+
+private static int i=0;
+  public static void main(String[] args) {
+    EvenOddNumbersThreads e = new EvenOddNumbersThreads();
+    Thread even = new Thread(() -> {
+      e.printEvenNumber();
+    });
+    Thread odd = new Thread(() -> {
+      e.printOddNumber();
+    });
+    even.start();
+    odd.start();
+  }
+
+  synchronized void printEvenNumber() {
+    while(i<10) {
+      try {
+        if (!(i % 2 == 0)) {
+
+          wait();
+        }
+        System.out.println(Thread.currentThread().getName()+ " "+i++);
+        notify();
+      } catch (InterruptedException e) {
+
+      }
+    }
+  }
+
+  synchronized void printOddNumber() {
+    while(i<10) {
+      try {
+        if (i % 2 == 0) {
+
+          wait();
+        }
+
+        System.out.println(Thread.currentThread().getName()+ " "+i++);
+        notify();
+      } catch (InterruptedException e) {
+
+      }
+    }
+  }
+}
